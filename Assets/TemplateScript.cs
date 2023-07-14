d23967db903ed251b53bcde3b20532be023599fd
KiloBites <90484876+KiloBites@users.noreply.github.com>
2023-07-14 16:55:22 -0400

    Initial commit

---
+++ Assets/TemplateScript.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+using static UnityEngine.Random;
+using static UnityEngine.Debug;
+
+public class TemplateScript : MonoBehaviour {
+
+	public KMBombInfo Bomb;
+	public KMAudio Audio;
+	public KMBombModule Module;
+
+	static int moduleIdCounter = 1;
+	int moduleId;
+	private bool moduleSolved;
+
+	void Awake()
+    {
+
+		moduleId = moduleIdCounter++;
+
+		/*
+		foreach (KMSelectable button in Buttons)
+			button.OnInteract() += delegate () { ButtonPress(button); return false; };
+		*/
+
+		//Button.OnInteract += delegate () { ButtonPress(); return false; };
+
+    }
+
+	
+	void Start()
+    {
+
+    }
+	
+	
+	void Update()
+    {
+
+    }
+
+	// Twitch Plays
+
+
+#pragma warning disable 414
+	private readonly string TwitchHelpMessage = @"!{0} something";
+#pragma warning restore 414
+
+	IEnumerator ProcessTwitchCommand (string command)
+    {
+		string[] split = command.ToUpperInvariant().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+		yield return null;
+    }
+
+	IEnumerator TwitchHandleForcedSolve()
+    {
+		yield return null;
+    }
+
+
+}
+
+
+
+
+
